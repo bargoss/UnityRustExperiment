c2df3eb541966c8048ce7bb3192b567230b43152
bargoss <yesiltepebaran98@gmail.com>
2022-11-10 22:44:44 +0300

    compiler optimizations

--- Plugin/build.sh
+++ Plugin/build.sh
@@ -1,8 +1,13 @@
 #mkdir -p ../Assets/Plugins/x86_64/
 #mkdir -p ../Assets/Plugins/WebGL/
-cargo build --target x86_64-pc-windows-msvc --release
+
+#cargo build --target x86_64-pc-windows-msvc --release
+#cargo build --target wasm32-unknown-unknown --release
+
+cargo rustc --target x86_64-pc-windows-msvc --release -- -C opt-level=3
+cargo rustc --target wasm32-unknown-unknown --release -- -C opt-level=3
+
 cp target/x86_64-pc-windows-msvc/release/mandelbrot.dll ../Assets/Plugins/x86_64/
-cargo build --target wasm32-unknown-unknown --release
 cp target/wasm32-unknown-unknown/release/libmandelbrot.a ../Assets/Plugins/WebGL/
 
 
