7bcd26211133d9e2d337c7c396448e090b4952da
bargoss <yesiltepebaran98@gmail.com>
2022-11-13 03:49:59 +0300

    dont display errors when failed to find the dlls to delete

--- Plugin/build_for_debug.py
+++ Plugin/build_for_debug.py
@@ -1,5 +1,5 @@
-#cargo rustc --target x86_64-pc-windows-msvc --release -- -C opt-level=3
-#cp target/x86_64-pc-windows-msvc/release/mandelbrot.dll ../
+# cargo rustc --target x86_64-pc-windows-msvc --release -- -C opt-level=3
+# cp target/x86_64-pc-windows-msvc/release/mandelbrot.dll ../
 import os
 import shutil
 
@@ -7,19 +7,16 @@ import shutil
 os.system("cargo rustc --target x86_64-pc-windows-msvc --release -- -C opt-level=3")
 shutil.copy("target/x86_64-pc-windows-msvc/release/mandelbrot.dll", "../")
 
-#../Assets/Plugins/x86_64/
-#../Assets/Plugins/WebGL/
+# ../Assets/Plugins/x86_64/
+# ../Assets/Plugins/WebGL/
 # clear stuff in these folders
-shutil.rmtree("../Assets/Plugins/x86_64/")
+try:
+    shutil.rmtree("../Assets/Plugins/x86_64/")
+except:
+    pass
 
-
-
-
-#os.system("cp target/x86_64-pc-windows-msvc/release/mandelbrot.dll ../")
+# os.system("cp target/x86_64-pc-windows-msvc/release/mandelbrot.dll ../")
 # in more pythonic way
 
-print("DoneDoneDoneDoneDoneDoneDoneDoneDoneDoneDoneDoneDoneDoneDoneDoneDoneDoneDoneDoneDoneDoneDoneDone")
-
-
-
-
+# get input from user
+input("Press Enter to continue...")
