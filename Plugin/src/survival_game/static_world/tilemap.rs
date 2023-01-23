c14c25f565b16783972b58a0d2f03a073f06fe59
bargoss <yesiltepebaran98@gmail.com>
2023-01-23 22:06:10 +0300

    quadtree experiment

--- Plugin/src/survival_game/static_world/tilemap.rs
+++ Plugin/src/survival_game/static_world/tilemap.rs
@@ -4,8 +4,8 @@
 // has occupying terrain entity id which is a uint
 
 
-
 use bevy_ecs::prelude::Component;
+use quadtree_rs::Quadtree;
 
 #[derive(Clone)]
 pub enum TileType{
@@ -57,5 +57,39 @@ impl TileMap {
 
     pub fn set_tile(&mut self, x: u32, y: u32, tile: Tile) {
         self.tiles[(y * self.width + x) as usize] = tile;
+    }
+}
+
+// create  tests
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_tilemap() {
+        use quadtree_rs::{area::AreaBuilder, point::Point, Quadtree};
+
+        // Instantiate a new quadtree which associates String values with u64 coordinates.
+        let mut qt = Quadtree::<u64, String>::new(/*depth=*/4);
+
+        // A depth of four means a square with width (and height) 2^4.
+        assert_eq!(qt.width(), 16);
+
+        // Associate the value "foo" with a rectangle of size 2x1, anchored at (0, 0).
+        let region_a = AreaBuilder::default()
+            .anchor(Point {x: 0, y: 0})
+            .dimensions((2, 1))
+            .build().unwrap();
+        qt.insert(region_a, "foo".to_string());
+
+        // Query over a region of size 2x2, anchored at (1, 0).
+        let region_b = AreaBuilder::default()
+            .anchor(Point {x: 1, y: 0})
+            .dimensions((2, 2))
+            .build().unwrap();
+        let mut query = qt.query(region_b);
+
+        // The query region (region_b) intersects the region "foo" is associated with (region_a), so the query iterator returns "foo" by reference.
+        assert_eq!(query.next().unwrap().value_ref(), "foo");
     }
 }
