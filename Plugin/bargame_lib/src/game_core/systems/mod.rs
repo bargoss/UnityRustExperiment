912a8dd300654f9b9e6890f1e30226e16f2113e1
bargoss <yesiltepebaran98@gmail.com>
2023-03-26 23:13:07 +0300

    refactor modules

--- Plugin/bargame_lib/src/game_core/systems/mod.rs
+++ Plugin/bargame_lib/src/game_core/systems/mod.rs
@@ -1,3 +1,8 @@
 pub mod physics_system;
+pub use physics_system::*;
+
 pub mod depend_on_rel_system;
+pub use depend_on_rel_system::*;
+
 pub mod id_entity_map_sync_system;
+pub use id_entity_map_sync_system::*;
