8fa19d776d73a9cd7f8c052a8d7fecd890ad481f
bargoss <yesiltepebaran98@gmail.com>
2023-03-29 21:11:12 +0300

    unit spawner system finalized

--- Plugin/bargame_lib/src/arena_fight_game/systems/unit_spawner_system.rs
+++ Plugin/bargame_lib/src/arena_fight_game/systems/unit_spawner_system.rs
@@ -2,16 +2,19 @@ use bevy_ecs::prelude::*;
 use crate::arena_fight_game::bundles::*;
 use crate::game_core::components::*;
 use crate::arena_fight_game::components::*;
+use crate::game_core::common::Id;
 use crate::game_core::math::*;
 use crate::game_core::resources::*;
 use crate::game_core::view_components::SphereView;
 
 fn unit_spawner_system(
-    mut commands: Commands,
+    commands: &mut Commands,
     time: Res<Time>,
     mut query: Query<(&mut UnitSpawner, &Position, &BelongsToFaction, &NetId)>,
     mut net_id_counter: ResMut<NetIdCounter>,
 ) {
+    let mut new_units = Vec::new();
+
     for (mut unit_spawner, position, belongs_to_faction, net_id) in query.iter_mut() {
         let time_fixed_point = time.fixed_delta_time * time.tick;
         let time_since_last_spawn = time_fixed_point - unit_spawner.last_spawn_time;
@@ -20,34 +23,74 @@ fn unit_spawner_system(
             // Update last_spawn_time to the current time
             unit_spawner.last_spawn_time = time_fixed_point;
 
-            // Spawn a new unit
-            let unit_net_id = net_id_counter.next();
-            let _ = commands.spawn(UnitBundle {
-                net_id: NetId { value: unit_net_id },
-                unit: Unit {
-                    target_movement_position: position.value + FixedPointV2::new(FixedPoint::new(0.1), FixedPoint::new(0.0))
-                },
-                character_movement: CharacterMovement::default(),
-                position: *position,
-                rigidbody: Rigidbody {
-                    velocity: FixedPointV2::default(),
-                    mass: FixedPoint::new(1.0),
-                },
-                impulse: Impulse::default(),
-                collider: CircleCollider {
-                    radius: FixedPoint::new(0.5),
-                },
-                health: Health {
-                    health: FixedPoint::new(5.0),
-                    max_health: FixedPoint::new(5.0),
-                    health_regen_per_second: FixedPoint::new(-0.025),
-                },
-                sphere_view: SphereView {
-                    radius: FixedPoint::new(0.5),
-                    view_custom_id: unit_net_id,
-                },
-                belongs_to_faction: *belongs_to_faction,
-            }).id();
+            let unit_spawn_position = position.value + FixedPointV2::new(FixedPoint::new(0.1), FixedPoint::new(0.0));
+            new_units.push(create_unit_creation_command(*net_id, unit_spawn_position, belongs_to_faction.faction));
         }
+    }
+
+    // sort by creator net_id and execute
+    new_units.sort_by(|a, b| a.creators_net_id.value.cmp(&b.creators_net_id.value));
+    for unit_creation_command in new_units {
+        execute_unit_creation_command(
+            unit_creation_command,
+             NetId{value: net_id_counter.next() },
+            commands
+        );
+    }
+}
+
+struct UnitCreationCommand {
+    creators_net_id: NetId,
+    unit_bundle: UnitBundle,
+}
+fn execute_unit_creation_command(
+    unit_creation_command: UnitCreationCommand,
+    net_id: NetId,
+    commands: &mut Commands,
+){
+    let mut bundle = unit_creation_command.unit_bundle;
+
+    bundle.net_id = net_id;
+    bundle.sphere_view.view_custom_id = net_id.value;
+
+    commands.spawn(bundle);
+}
+
+fn create_unit_creation_command(
+    creators_net_id : NetId,
+    position: FixedPointV2,
+    faction: Faction
+) -> UnitCreationCommand
+{
+    let bundle = UnitBundle {
+        net_id: NetId { value: Id::new(0) },
+        unit: Unit {
+            target_movement_position: position
+        },
+        character_movement: CharacterMovement::default(),
+        position: Position{value: position},
+        rigidbody: Rigidbody {
+            velocity: FixedPointV2::default(),
+            mass: FixedPoint::new(1.0),
+        },
+        impulse: Impulse::default(),
+        collider: CircleCollider {
+            radius: FixedPoint::new(0.5),
+        },
+        health: Health {
+            health: FixedPoint::new(5.0),
+            max_health: FixedPoint::new(5.0),
+            health_regen_per_second: FixedPoint::new(-0.025),
+        },
+        sphere_view: SphereView {
+            radius: FixedPoint::new(0.5),
+            view_custom_id: Id::new(0),
+        },
+        belongs_to_faction: BelongsToFaction{faction},
+    };
+
+    UnitCreationCommand {
+        creators_net_id,
+        unit_bundle: bundle,
     }
 }
