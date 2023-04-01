08b3261f918aa09863ba35a871d6ef0e10fbfe0c
bargoss <yesiltepebaran98@gmail.com>
2023-04-01 13:53:46 +0300

    unit_capture_node_system conflicts fixed

--- Plugin/bargame_lib/src/arena_fight_game/systems/unit_capture_node_system.rs
+++ Plugin/bargame_lib/src/arena_fight_game/systems/unit_capture_node_system.rs
@@ -1,5 +1,6 @@
 use std::collections::HashMap;
 use bevy_ecs::prelude::*;
+use bevy_ecs::query::QueryEntityError;
 use nalgebra::DimAdd;
 use crate::arena_fight_game::components::*;
 use crate::game_core::common::Id;
@@ -10,12 +11,13 @@ use crate::game_core::verlet_physics::verlet_physics_world::VerletPhysicsWorld;
 
 
 pub fn unit_capture_node_system(
-    mut capturing_unit_query: Query<(&mut Health, &Position, &BelongsToFaction, &NetId, &CircleCollider)>,
-    mut node_query: Query<(Entity, &mut Node, &Position, &mut BelongsToFaction, &NetId, &CircleCollider)>,
+    mut capturing_unit_query: Query<(Entity, &mut Health, &Position, &NetId, &CircleCollider)>,
+    mut node_query: Query<(Entity, &mut Node, &Position, &NetId, &CircleCollider)>,
+    mut belongs_to_faction_query: Query<(Entity, &mut BelongsToFaction)>,
     physics_world: Res<VerletPhysicsWorld>,
     id_entity_map: Res<IdEntityMap>,
     time: Res<Time>,
-){
+) {
     let capture_per_second = FixedPoint::new(0.05);
     let capture_per_frame = time.fixed_delta_time * capture_per_second;
     let capture_range = FixedPoint::new(1.0);
@@ -23,59 +25,63 @@ pub fn unit_capture_node_system(
     let unit_health_loss_per_second = FixedPoint::new(0.5);
     let unit_health_loss_per_frame = time.fixed_delta_time * unit_health_loss_per_second;
 
-    // Collect entities and their NetIds into a vector
-    let mut entities_and_net_ids: Vec<(Entity, NetId)> = node_query.iter().map(|(entity, _, _, _, net_id, _)| (entity, *net_id)).collect();
-
-    // Sort the vector by NetId
-    entities_and_net_ids.sort_by_key(|(_, net_id)| *net_id);
-
-
     let mut nearby_bodies_query_buffer = Vec::new();
 
-    // Iterate through the sorted entities
-    for (entity, _) in entities_and_net_ids {
-        // Get the node components for each entity
-        if let Ok((_, mut node, node_position, mut node_faction, node_net_id, node_collider)) = node_query.get_mut(entity) {
-            // Get the entities that are overlapping the node
-            physics_world.overlap_circle(node_position.value, node_collider.radius + FixedPoint::new(0.1), &mut nearby_bodies_query_buffer);
+    // iterate node_query
+    for (node_entity, mut node, node_position, node_net_id, node_collider) in node_query.iter_mut() {
+        physics_world.overlap_circle(node_position.value, capture_range + node_collider.radius, &mut nearby_bodies_query_buffer);
 
-            // Iterate through the entities that are overlapping the node
-            for body_id in nearby_bodies_query_buffer.iter() {
-                // Get the capturing unit components
-                if let Some((mut capturing_unit, capturing_unit_position, capturing_unit_faction, capturing_unit_net_id, capturing_unit_collider)) = id_entity_map.get_mut_from_query(&mut capturing_unit_query, Id::new(*body_id)) {
-                    // Check if the capturing unit is on the same faction as the node
-                    if capturing_unit_faction.faction != node_faction.faction {
-                        // Check if the capturing unit is within range of the node
-                        let distance = (capturing_unit_position.value - node_position.value).magnitude_squared();
-                        if distance < capture_range * capture_range {
-                            // Decrease the capturing unit's health
-                            capturing_unit.health -= unit_health_loss_per_frame;
-                            // Decrease the node's capture progress
-                            progress_node_capture_progress(capture_per_frame, capturing_unit_faction.faction, &mut node, &mut node_faction);
-                        }
+        // iterate nearby_bodies_query_buffer
+        for body_id in nearby_bodies_query_buffer.iter() {
+            match id_entity_map.get(Id::new(*body_id)) {
+                Some(unit_entity) => {
+                    if let Ok(captuing_unit) = capturing_unit_query.get_mut(unit_entity) {
+                        // unit is capturing node
+                        let node_belongs_to_faction = belongs_to_faction_query.get(node_entity).unwrap().1;
+                        let capturing_unit_belongs_to_faction = belongs_to_faction_query.get(unit_entity).unwrap().1;
+                        let node_clone = node.clone();
+                        let (updated_node_belongs_to_faction, updated_node) = progress_node_capture_progress(capture_per_frame, capturing_unit_belongs_to_faction.faction, &node_clone, node_belongs_to_faction);
+                        *node = updated_node;
+                        let mut node_belongs_to_faction = belongs_to_faction_query.get_mut(node_entity).unwrap().1;
+                        *node_belongs_to_faction = updated_node_belongs_to_faction;
+
+                        let mut capturing_unit_health = captuing_unit.1;
+                        capturing_unit_health.health -= unit_health_loss_per_frame;
                     }
                 }
+                None => {continue}
             }
         }
+
+
+
     }
+
+
+
 }
 
-fn progress_node_capture_progress(capture_amount: FixedPoint, capturing_faction: Faction, target_node: &mut Node, belongs_to_faction: &mut BelongsToFaction) {
-    if target_node.capture_progress_faction != capturing_faction {
-        // decrease progress
-        target_node.capture_progress -= capture_amount;
-        if target_node.capture_progress < FixedPoint::zero() {
-            target_node.capture_progress = FixedPoint::zero();
-            target_node.capture_progress_faction = capturing_faction;
+fn progress_node_capture_progress(capture_amount: FixedPoint, capturing_faction: Faction, target_node: &Node, belongs_to_faction: &BelongsToFaction) -> (BelongsToFaction, Node)
+{
+    let mut updated_node = target_node.clone();
+    let mut updated_belongs_to_faction = belongs_to_faction.clone();
+
+    if updated_node.capture_progress_faction != capturing_faction {
+        updated_node.capture_progress -= capture_amount;
+        if updated_node.capture_progress < FixedPoint::zero() {
+            updated_node.capture_progress = FixedPoint::zero();
+            updated_node.capture_progress_faction = capturing_faction;
         }
     }
     else{
         // increase progress
-        target_node.capture_progress += capture_amount;
-        if target_node.capture_progress > FixedPoint::one() {
-            target_node.capture_progress = FixedPoint::one();
-            target_node.capture_progress_faction = capturing_faction;
-            belongs_to_faction.faction = capturing_faction;
+        updated_node.capture_progress += capture_amount;
+        if updated_node.capture_progress > FixedPoint::one() {
+            updated_node.capture_progress = FixedPoint::one();
+            updated_node.capture_progress_faction = capturing_faction;
+            updated_belongs_to_faction.faction = capturing_faction;
         }
     }
+
+    (updated_belongs_to_faction, updated_node)
 }
