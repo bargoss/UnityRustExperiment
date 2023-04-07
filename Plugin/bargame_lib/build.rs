ca5a58c09731826c41a8cce396544f162b809025
Baran Yesiltepe <byesiltepe@zynga.com>
2023-04-07 11:52:15 +0300

    refactor

--- Plugin/bargame_lib/build.rs
+++ Plugin/bargame_lib/build.rs
@@ -1,42 +1,32 @@
 use std::env;
 use std::path::PathBuf;
 use std::process::Command;
-use std::io::Write;
 
 fn main() {
-    let mut file = std::fs::OpenOptions::new()
-        .write(true)
-        .append(true)
-        .create(true)
-        .open("example_txt_in_project_folder.txt")
-        .unwrap();
-
-    // Compile protobuf for C#
     let manifest_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
-
-    let protoc_executable = PathBuf::from(&manifest_dir).join("protoc.exe");
-    env::set_var("PROTOC", protoc_executable.clone());
-
     let proto_dir = PathBuf::from(&manifest_dir).join("proto");
 
-    let csharp_out_dir = PathBuf::from(&manifest_dir).join("csharp\\Generated");
+    compile_protobuf_for_csharp(&manifest_dir, &proto_dir);
+    compile_protobuf_for_rust(&manifest_dir, &proto_dir);
+}
 
-    let mut binding = Command::new(&protoc_executable.clone());
-    let command = binding
+fn compile_protobuf_for_csharp(manifest_dir: &str, proto_dir: &PathBuf) {
+    let protoc_executable = PathBuf::from(manifest_dir).join("protoc.exe");
+    env::set_var("PROTOC", &protoc_executable);
+
+    let csharp_out_dir = PathBuf::from(manifest_dir).join("csharp\\Generated");
+
+    Command::new(&protoc_executable)
         .arg(format!("--proto_path={}", proto_dir.display()))
         .arg(format!("--csharp_out={}", csharp_out_dir.display()))
-        .arg(proto_dir.join("example.proto"));
+        .arg(proto_dir.join("example.proto"))
+        .status()
+        .expect("Failed to execute protoc");
+}
 
-    let status = command.status().expect("Failed to execute protoc");
-
-    writeln!(file, "status: {:?}", command).unwrap();
-
-    // Compile protobuf for Rust
-    let rust_out_dir = PathBuf::from(&manifest_dir).join("src/protos");
-
-    if !rust_out_dir.exists() {
-        std::fs::create_dir_all(&rust_out_dir).unwrap();
-    }
+fn compile_protobuf_for_rust(manifest_dir: &str, proto_dir: &PathBuf) {
+    let rust_out_dir = PathBuf::from(manifest_dir).join("src/protos");
+    std::fs::create_dir_all(&rust_out_dir).unwrap_or_default();
 
     prost_build::Config::new()
         .out_dir(&rust_out_dir)
